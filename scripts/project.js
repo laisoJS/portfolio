6600a94e1d88e3f98b104ab1ce97dfcf696fe707
laiso <aloisleroy2016@gmail.com>
2024-08-10 15:43:41 +0200

    Changing the text back to default on mouse out

--- scripts/project.js
+++ scripts/project.js
@@ -1,18 +1,28 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const projectImgs = document.querySelectorAll(".projectImg");
+    document.addEventListener("DOMContentLoaded", () => {
+        const projectImgs = document.querySelectorAll(".projectImg");
 
-    const articleHeading = document.querySelector("article h2"),
-          articleParagraph = document.querySelector("article p");
-    
-    projectImgs.forEach((img, index) => {
-            // Stack card
-            img.style.marginLeft = `${(index - 1) * 13}%`;
-            img.style.marginTop = "-40dvh";
+        const articleHeading = document.querySelector("article h2"),
+            articleParagraph = document.querySelector("article p");
 
-            // Change the h2 and p tag in article
-            img.addEventListener("mouseover", () => {
-                articleHeading.textContent = img.alt;
-                articleParagraph.textContent = img.getAttribute("data-story");
-            });
+        // Store the default text
+        const defaultHeading = articleHeading.textContent;
+        const defaultParagraph = articleParagraph.textContent;
+        
+        projectImgs.forEach((img, index) => {
+                // Stack card
+                img.style.marginLeft = `${(index - 1) * 13}%`;
+                img.style.marginTop = "-40dvh";
+
+                // Change the h2 and p tag in article
+                img.addEventListener("mouseover", () => {
+                    articleHeading.textContent = img.alt;
+                    articleParagraph.textContent = img.getAttribute("data-story");
+                });
+
+                // Change the text back to the default one when the mouse leave
+                img.addEventListener("mouseout", () => {
+                    articleHeading.textContent = defaultHeading
+                    articleParagraph.textContent = defaultParagraph
+                });
+        });
     });
-});
